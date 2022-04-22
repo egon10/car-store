b7ee6d17fe15f9b1b1d87940b6d6b4f3bc8004e8
Egon <104030578+egon10@users.noreply.github.com>
2022-04-22 21:30:32 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
 export const MONGO_DB_URL =
   process.env.MONGO_DB_URL ||
   'mongodb+srv://egon:MGkqhcU1EvdLXJl8@cluster0.g65hj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-// egon
-// MGkqhcU1EvdLXJl8
