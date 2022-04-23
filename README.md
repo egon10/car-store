2ffeefcfb0ea8357af7a1a592f403f8e87286643
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 12:07:22 +0200

    use nestjs config with .env properties

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## Car Store
+
+### Local setup
+
+- In the `/backend` folder, create a file called `.env` and copy the properties from `.env.sample` with the correct values
+
+- Run `npm install`
+
+- Start the application with `npm start`
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import { MONGO_DB_URL } from '../config';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> => mongoose.connect(MONGO_DB_URL)
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect(process.env.MONGO_DB_URL)
   }
 ];
