da61811f7c586f24e0dc2f61ebe1ba2212ce8836
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 11:45:26 +0200

    remove hardcoded db url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,1 @@
-export const MONGO_DB_URL =
-  process.env.MONGO_DB_URL ||
-  'mongodb+srv://egon:MGkqhcU1EvdLXJl8@cluster0.g65hj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+export const MONGO_DB_URL = process.env.MONGO_DB_URL;
